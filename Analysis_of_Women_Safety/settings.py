c4bfbe6e691f5e4c6d558749785d4bb39614b53d
Narasimha Reddy <narasimha.depl@gmail.com>
2021-02-12 16:57:42 +0530

    changes for wsgi

--- Analysis_of_Women_Safety/settings.py
+++ Analysis_of_Women_Safety/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
+STATIC_ROOT = '/home/narasimha/womensafety/design/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'design/images')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'design/media')
